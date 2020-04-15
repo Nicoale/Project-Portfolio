edbd7ecc6b823d191bf026f7a6d2dfbdeaf92390
nicoale <nicoale8786@gmail.com>
2020-04-15 12:49:27 +0300

    Removed node modules

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,10 @@ var watch= require("gulp-watch");
 sass.compiler = require("node-sass");
 
 gulp.task('sass', function () {
-    //watch('.sass/**/*.scss',function(){ 
-        return  gulp.src('./sass/**/*.scss') //take the files in sass directory
+        return watch('.sass/**/*.scss',function(){ 
+         gulp.src('./sass/**/*.scss') //take the files in sass directory
         .pipe(sass().on('error' , sass.logError)) // convert to csss and log any error if there is
         .pipe(gulp.dest('./public/css')); // put the resulting files inside css directory
-//})
-});
-gulp.task('sass', function () {
-        return  gulp.src('./sass/**/*.scss') //take the files in sass directory
-        .pipe(sass().on('error' , sass.logError)) // convert to csss and log any error if there is
-        .pipe(gulp.dest('./public/css')); // put the resulting files inside css directory
+        })
 });
 
