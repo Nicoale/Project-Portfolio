dfafe52c17aa8b66047f5ecbe1c53aebe8b4984b
nicoale <nicoale8786@gmail.com>
2020-04-15 09:00:39 +0300

    changed the tests path to visit

--- cypress/integration/examples/deposits_spec.js
+++ cypress/integration/examples/deposits_spec.js
@@ -1,7 +1,7 @@
 describe ('Deposits Test',function(){
     it('Checks for any available deposit',function(){
         // expect(true).to.equal(true)
-        cy.visit('https://example.cypress.io')
+        cy.visit('localhost:9292/deposits')
         cy.contains('text').click()
         cy.type('1000')
           .should('have.value','1000')
