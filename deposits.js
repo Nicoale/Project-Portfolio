dc9da763398aff7ae8b8cd37f65de7cd7b8d7aef
nicoale <nicoale8786@gmail.com>
2020-04-16 08:47:20 +0300

    completed the then part of fetch

--- deposits.js
+++ deposits.js
@@ -1,6 +1,5 @@
-  function getDetails(){
-    let dname = document.getElementById("name");
-    let damount=document.getElementById("amount");
-    fetch("/deposit", {method: "POST", {name: dname, amount: damount}}).then(()=>{})
-
-}
+function getDetails() { 
+    let dname = document.getElementById("name"); 
+    let damount = document.getElementById("amount"); 
+    fetch("/deposit", {method: "POST",body: { name: dname, amount: damount}}).then((data) => {console.log(data)});
+ }
