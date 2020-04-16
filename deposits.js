1a30ea412d2dec1fd6e667e0f81d682626dc7ca2
nicoale <nicoale8786@gmail.com>
2020-04-16 12:17:41 +0300

    ensure the form values are picked and sent to the server

--- deposits.js
+++ deposits.js
@@ -1,5 +1,15 @@
+const toUrlEncoded = obj => Object.keys(obj).map(k => encodeURIComponent(k) + '=' + encodeURIComponent(obj[k])).join('&');
 function getDetails() { 
-    let dname = document.getElementById("name"); 
-    let damount = document.getElementById("amount"); 
-    fetch("/deposit", {method: "POST",body: { name: dname, amount: damount}}).then((data) => {console.log(data)});
+    let dname = document.getElementById("name").value; 
+    let damount = document.getElementById("amount").value; 
+    fetch("/deposit", {
+        method:"POST",
+        headers:{
+            'Content-Type':'application/x-www-form-urlencoded',
+        },
+        body: toUrlEncoded({ name: dname, amount: damount})
+    })
+.then((data) => {
+            console.log(data)
+        })
  }
